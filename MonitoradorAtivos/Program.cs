5ed2bbe06ee445878ed3bd8bda16e001dd5d10fd
JoãoVitorAlmeida <joao.vta@gmail.com>
2023-07-25 02:11:50 -0300

    Implemented email sending using sendgrid

--- MonitoradorAtivos/Program.cs
+++ MonitoradorAtivos/Program.cs
@@ -1,13 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.IO.Pipes;
-using System.Net;
-using Newtonsoft.Json;
+﻿using Newtonsoft.Json;
 using Microsoft.Extensions.Configuration;
-using MailKit.Net.Smtp;
-using MailKit.Security;
-using MimeKit;
+using SendGrid;
+using SendGrid.Helpers.Mail;
 
 HttpClient httpClient = new HttpClient();
 
@@ -23,49 +17,36 @@ string SOURCE_EMAIL = configuration["source_email:email"];
 string SOURCE_EMAIL_NAME = configuration["source_email:name"];
 string SOURCE_EMAIL_API_KEY = configuration["source_email:key"];
 
+
 async Task<double> GetLastAssetPrice(string asset)
 {
-    string QUERY_URL = String.Format("https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol={0}&interval=5min&apikey={1}", asset, API_KEY);
+    string QUERY_URL = String.Format("https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol={0}&interval=5min&apikey={1}",
+        asset, API_KEY);
     string responseBody = await httpClient.GetStringAsync(QUERY_URL);
 
     dynamic parsedResponse = JsonConvert.DeserializeObject(responseBody);
     dynamic parsedData = parsedResponse["Time Series (5min)"];
-    SortedDictionary<DateTime, Dictionary<string, double>> formattedData = JsonConvert.DeserializeObject<SortedDictionary<DateTime, Dictionary<string, double>>>(parsedData.ToString());
+    SortedDictionary<DateTime, Dictionary<string, double>> formattedData =
+        JsonConvert.DeserializeObject<SortedDictionary<DateTime, Dictionary<string, double>>>(parsedData.ToString());
     double lastAssetPrice = formattedData.Last().Value["4. close"];
 
     return lastAssetPrice;
 }
 
-async void SendEmail(string title, string body)
+
+void SendEmail(string title, string body)
 {
-    using var message = new MimeMessage();
-    message.From.Add(new MailboxAddress(
-        SOURCE_EMAIL,
-        SOURCE_EMAIL_NAME
-    ));
-    message.To.Add(new MailboxAddress(
-        DEST_EMAIL,
-        DEST_EMAIL_NAME
-    ));
-    message.Subject = title;
-    var bodyBuilder = new BodyBuilder
-    {
-        TextBody = body
-    };
-    message.Body = bodyBuilder.ToMessageBody();
+    var subject = title;
+    var plainTextContent = body;
 
+    var client = new SendGridClient(SOURCE_EMAIL_API_KEY);
+    var from_email = new EmailAddress(SOURCE_EMAIL, SOURCE_EMAIL_NAME);
+    var to_email = new EmailAddress(DEST_EMAIL, DEST_EMAIL_NAME);
 
-    using var smtpCLient = new SmtpClient();
-    //smtpCLient.ConnectAsync("smtp.sendgrid.net", 587, SecureSocketOptions.StartTls);
-    //smtpCLient.AuthenticateAsync(
-    //    userName: "apikey",
-    //    password: SOURCE_EMAIL_API_KEY 
-    //);
-
-    Console.WriteLine("Sending email");
-    //await smtpCLient.SendAsync(message);
-    Console.WriteLine("Email sent");
+    var msg = MailHelper.CreateSingleEmail(from_email, to_email, subject, plainTextContent, "");
+    client.SendEmailAsync(msg);
 }
+
 
 string[] arguments = Environment.GetCommandLineArgs();
 Console.WriteLine(arguments.Length);
@@ -87,10 +68,11 @@ try
 catch (FormatException)
 {
     Console.WriteLine("Error: invalid sell or purchase price");
-    throw new InvalidDataException();
+    throw new InvalidOperationException();
 }
 
 Console.WriteLine("Tracking asset {0}. Advice set to sell above R${1} and to buy beneath R${2}", asset, sellPrice, purchasePrice);
+
 
 while (true)
 {
@@ -100,15 +82,17 @@ while (true)
     if (currentPrice > sellPrice)
     {
         Console.WriteLine("Asset {0} surpassed R${1}. Advice is to sell.", asset, sellPrice);
+        SendEmail("Advice to sell asset", String.Format("Hello! You should probably sell asset {0}. " +
+            "It is currenlty worth {1}, more than the set price {2}.", asset, currentPrice, sellPrice));
     }
 
     if (currentPrice < purchasePrice)
     {
         Console.WriteLine("Asset {0} dropped bellow R${1}. Advice is to buy.", asset, purchasePrice);
+        SendEmail("Advice to buy asset", String.Format("Hello! You should probably buy asset {0}. " +
+            "It is currenlty worth {1}, more than the set price {2}.", asset, currentPrice, purchasePrice));
     }
 
-    SendEmail("Test email", "test");
-    Thread.Sleep(1000*60);
-    break;
-
+    // Sleep set to 5 minutes due to API limitations
+    Thread.Sleep(1000*60*5);
 }
